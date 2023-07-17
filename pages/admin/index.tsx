e60278d363fae62e99da7f361ff6a9e2105c0281
takapc <kota03240213@gmail.com>
2023-07-17 15:13:20 +0900

    Add admin/accounts filter

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,12 +1,25 @@
-import { Button, Card, CardBody, CardFooter, CardHeader, Heading, Link, SimpleGrid, Text, VStack } from "@chakra-ui/react"
-import { GetServerSideProps, NextPage } from "next"
-import { parseCookies, destroyCookie } from "nookies"
-import Router from 'next/router'
+import {
+    Button,
+    Card,
+    CardBody,
+    CardFooter,
+    CardHeader,
+    Heading,
+    Link,
+    SimpleGrid,
+    Text,
+    VStack,
+} from "@chakra-ui/react";
+import { GetServerSideProps, NextPage } from "next";
+import { parseCookies, destroyCookie } from "nookies";
+import Router, { useRouter } from "next/router";
 
 const Page: NextPage = () => {
+    const router = useRouter();
+
     return (
         <VStack>
-            <SimpleGrid columns={[1,3]} spacing="40px" margin="20px">
+            <SimpleGrid columns={[1, 3]} spacing="40px" margin="20px">
                 <Card width={200}>
                     <CardHeader>
                         <Heading size={"md"}>決済</Heading>
@@ -15,9 +28,12 @@ const Page: NextPage = () => {
                         <Text>QRコードを読み込んで決済する</Text>
                     </CardBody>
                     <CardFooter>
-                        <Button bgColor={"yellow.400"} onClick={() => {
-                            window.open('/', '決済', 'popup=true')
-                        }}>
+                        <Button
+                            bgColor={"yellow.400"}
+                            onClick={() => {
+                                window.open("/", "決済", "popup=true");
+                            }}
+                        >
                             Click
                         </Button>
                     </CardFooter>
@@ -27,12 +43,12 @@ const Page: NextPage = () => {
                         <Heading size={"md"}>ユーザー</Heading>
                     </CardHeader>
                     <CardBody>
-                        <Text>QRコードを読み取ってユーザーの情報を確認する</Text>
+                        <Text>
+                            QRコードを読み取ってユーザーの情報を確認する
+                        </Text>
                     </CardBody>
                     <CardFooter>
-                        <Button bgColor={"purple.400"}>
-                            Click
-                        </Button>
+                        <Button bgColor={"purple.400"}>Click</Button>
                     </CardFooter>
                 </Card>
                 <Card width={200}>
@@ -43,9 +59,7 @@ const Page: NextPage = () => {
                         <Text>店舗の追加や削除をする</Text>
                     </CardBody>
                     <CardFooter>
-                        <Button bgColor={"lightgreen"}>
-                            Click
-                        </Button>
+                        <Button bgColor={"lightgreen"}>Click</Button>
                     </CardFooter>
                 </Card>
                 <Card width={200}>
@@ -56,7 +70,10 @@ const Page: NextPage = () => {
                         <Text>ユーザーアカウントの作成や削除をする</Text>
                     </CardBody>
                     <CardFooter>
-                        <Button bgColor={"lightblue"}>
+                        <Button
+                            bgColor={"lightblue"}
+                            onClick={() => router.push("/admin/accounts")}
+                        >
                             Click
                         </Button>
                     </CardFooter>
@@ -69,9 +86,7 @@ const Page: NextPage = () => {
                         <Text>サーバーの負荷状況や運営の損失を確認する</Text>
                     </CardBody>
                     <CardFooter>
-                        <Button bgColor={"blue.500"}>
-                            Click
-                        </Button>
+                        <Button bgColor={"blue.500"}>Click</Button>
                     </CardFooter>
                 </Card>
                 <Card width={200}>
@@ -82,34 +97,39 @@ const Page: NextPage = () => {
                         <Text>管理アカウントをログアウトする</Text>
                     </CardBody>
                     <CardFooter>
-                        <Button bgColor={"red.400"} onClick={() => {
-                            destroyCookie(null, 'idToken')
-                            Router.push("/admin/login")
-                        }}>
+                        <Button
+                            bgColor={"red.400"}
+                            onClick={() => {
+                                destroyCookie(null, "idToken");
+                                Router.push("/admin/login");
+                            }}
+                        >
                             Click
                         </Button>
                     </CardFooter>
                 </Card>
             </SimpleGrid>
         </VStack>
-    )
-}
+    );
+};
 
-export const getServerSideProps: GetServerSideProps<{hoge: null}> = async ctx => {
-    const idToken = parseCookies(ctx).idToken
+export const getServerSideProps: GetServerSideProps<{ hoge: null }> = async (
+    ctx
+) => {
+    const idToken = parseCookies(ctx).idToken;
     if (!idToken) {
         return {
             redirect: {
                 permanent: false,
-                destination: '/admin/login'
-            }
-        }
+                destination: "/admin/login",
+            },
+        };
     }
     return {
         props: {
-            hoge: null
-        }
-    }
-}
+            hoge: null,
+        },
+    };
+};
 
-export default Page
+export default Page;
