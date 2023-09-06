5a5c8945beb543e3b0c3a2093dbbcd9b5d67709d
takapc <kota03240213@gmail.com>
2023-09-06 16:09:32 +0900

    fix3

--- components/TransactionItem.tsx
+++ components/TransactionItem.tsx
@@ -7,7 +7,7 @@ export const TransactionItem = (props: {
 }) => {
     let amountString = "± 0";
     let amountColor = "yellow.400";
-    if (props.type == "") {
+    if (props.type == "payout") {
         amountString = "- " + Math.abs(props.amount).toLocaleString();
         amountColor = "red.400";
     } else {
